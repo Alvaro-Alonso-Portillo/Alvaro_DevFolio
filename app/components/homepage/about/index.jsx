c99d081e83a5891830de61abe74100b4199b564c
Alvaro-Alonso-Portillo <balonsomorillo@gmail.com>
2025-02-20 12:16:50 +0100

    fix: actualizar dimensiones de la imagen de perfil en la sección Acerca de

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -28,8 +28,8 @@ function AboutSection() {
         <div className="flex justify-center order-1 lg:order-2">
           <Image
             src={personalData.profile}
-            width={360}
-            height={360}
+            width={620}
+            height={620}
             alt="Abu Said"
             className="rounded-lg transition-all duration-1000 grayscale hover:scale-110 hover:grayscale-0 cursor-pointer object-cover"
           />
